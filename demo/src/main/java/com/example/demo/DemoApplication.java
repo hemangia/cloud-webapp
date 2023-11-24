cd8cd7752ab257468f236fa52831712e5e6af435
Hemangi Ramesh Ahire <ahire.h@northeastern.edu>
2023-11-24 01:50:07 -0800

    Changes for assign9-5 csv email changes

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -33,7 +33,7 @@ public class DemoApplication {
 	        AccountService accountService = context.getBean(AccountService.class);
 
 	        try {
-	           System.out.println("Hello world test 2 dev 27");
+	           System.out.println("Hello world test 9 dev 5");
 	            Resource resource = context.getResource("classpath:inputfiles/users.csv");
 	            InputStream inputStream = resource.getInputStream();
 
