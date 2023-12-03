c29ab4d92885e88fb62a625c123e13ba061c0484
Hemangi Ramesh Ahire <ahire.h@northeastern.edu>
2023-12-03 15:54:58 -0800

    Changes for assign 10 dev15

--- demo/src/main/java/com/example/demo/controller/AssignmentController.java
+++ demo/src/main/java/com/example/demo/controller/AssignmentController.java
@@ -52,7 +52,7 @@ import software.amazon.awssdk.services.sns.model.Topic;
 
 
 @RestController
-@RequestMapping("/v11/assignments")
+@RequestMapping("/v15/assignments")
 public class AssignmentController {
 	@Autowired
 	private AssignmentRepository assignmentRepository;
