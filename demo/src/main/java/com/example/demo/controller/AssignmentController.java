cd7bda0d32402d1df1a42206709f0d7e295190f9
Hemangi Ramesh Ahire <ahire.h@northeastern.edu>
2023-12-04 10:05:02 -0800

    changes for dev29

--- demo/src/main/java/com/example/demo/controller/AssignmentController.java
+++ demo/src/main/java/com/example/demo/controller/AssignmentController.java
@@ -52,7 +52,7 @@ import software.amazon.awssdk.services.sns.model.Topic;
 
 
 @RestController
-@RequestMapping("/v28/assignments")
+@RequestMapping("/v29/assignments")
 public class AssignmentController {
 	@Autowired
 	private AssignmentRepository assignmentRepository;
